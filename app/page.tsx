93ac97f5e8362ac6fa10d082b1e12543ff7e908f
Awet Tsegazeab <avvvet@gmail.com>
2023-07-28 09:23:19 -0400

    triger version 1.0.4

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            fiscally version 1.0.0 {' '}
+            fiscally version 1.0.4 {' '}
             <Image
               src="/vercel.svg"
               alt="Vercel Logo"
